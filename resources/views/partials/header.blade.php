6ac57ee25901af83e81015da6e679b3247a8d15e
shaikhalamin <shaikhalamincse@gmail.com>
2016-10-19 04:11:50 +0600

    Home Icon added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,8 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Ecommerce</a>
+      <a class="navbar-brand" href="/"><i class="fa fa-home" aria-hidden="true"></i>
+Ecommerce</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
